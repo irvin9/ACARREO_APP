2e898d4e607d6ec927914f2b60efc99a381f90f6
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-09-01 09:16:18 -0600

    Add usefull thirdActivity

--- app/src/main/java/com/example/acarreo_app/ACARREO_FILE/SecondActivity.kt
+++ app/src/main/java/com/example/acarreo_app/ACARREO_FILE/SecondActivity.kt
@@ -1,12 +1,22 @@
 package com.example.acarreo_app.ACARREO_FILE
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 import com.example.acarreo_app.R
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val nextButton = findViewById<AppCompatButton>(R.id.nextButton)
+
+        nextButton.setOnClickListener{
+            val pantalla3 = Intent(this, ThirdActivity::class.java)
+            startActivity(pantalla3)
+        }
     }
 }
