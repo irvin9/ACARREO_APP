e07737ca7614ab7002182fa4712400e71268ea07
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-09-01 09:23:17 -0600

    Add ScanActivity empty

---
+++ app/src/main/java/com/example/acarreo_app/ACARREO_FILE/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.acarreo_app.ACARREO_FILE
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.acarreo_app.R
+
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
--- app/src/main/java/com/example/acarreo_app/ACARREO_FILE/ThirdActivity.kt
+++ app/src/main/java/com/example/acarreo_app/ACARREO_FILE/ThirdActivity.kt
@@ -1,12 +1,21 @@
 package com.example.acarreo_app.ACARREO_FILE
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 import com.example.acarreo_app.R
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val module1Button = findViewById<AppCompatButton>(R.id.module1Button)
+
+        module1Button.setOnClickListener{
+            val scanScreen = Intent(this, ScanActivity::class.java)
+            startActivity(scanScreen)
+        }
     }
 }
