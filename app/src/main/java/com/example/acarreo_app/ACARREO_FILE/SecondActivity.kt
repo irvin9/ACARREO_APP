ad8826828096d380788c54c94a3abcb9d14cd6aa
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-09-05 09:53:25 -0600

    Add control for descriptionInput visibility

--- app/src/main/java/com/example/acarreo_app/ACARREO_FILE/SecondActivity.kt
+++ app/src/main/java/com/example/acarreo_app/ACARREO_FILE/SecondActivity.kt
@@ -1,20 +1,48 @@
 package com.example.acarreo_app.ACARREO_FILE
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatSpinner
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import android.view.View
+import android.widget.AdapterView
 import com.example.acarreo_app.R
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         val nextButton = findViewById<AppCompatButton>(R.id.nextButton)
+        val locationSpinner = findViewById<AppCompatSpinner>(R.id.locationSpinner)
+        val descriptionInput = findViewById<AppCompatEditText>(R.id.descriptionInput)
+
+        val selectedLocation = locationSpinner.selectedItem.toString()
+
+        locationSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedLocation = locationSpinner.selectedItem.toString()
+                    if (selectedLocation == "OTRO") {
+                        descriptionInput.isVisible = true
+                    } else {
+                        descriptionInput.isVisible = false
+                    }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // No se seleccionó ningún elemento
+            }
+        }
 
         nextButton.setOnClickListener{
+
             val pantalla3 = Intent(this, ThirdActivity::class.java)
             startActivity(pantalla3)
         }
