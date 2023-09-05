c86900532fbfab8ac30c309da5041be75046bf0e
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-09-04 20:41:59 -0600

    Add void nfc activity

---
+++ app/src/main/java/com/example/acarreo_app/ACARREO_FILE/nfcActivity.kt
@@ -0,0 +1,12 @@
+package com.example.acarreo_app.ACARREO_FILE
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.acarreo_app.R
+
+class nfcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
+    }
+}
