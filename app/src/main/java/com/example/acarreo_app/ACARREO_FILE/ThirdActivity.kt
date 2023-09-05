325857ebf9de763fc6a067d711e78663b9acaf21
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-09-05 12:45:00 -0600

    Add glide for gif view

--- app/src/main/java/com/example/acarreo_app/ACARREO_FILE/ThirdActivity.kt
+++ app/src/main/java/com/example/acarreo_app/ACARREO_FILE/ThirdActivity.kt
@@ -3,7 +3,9 @@ package com.example.acarreo_app.ACARREO_FILE
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatButton
+import com.bumptech.glide.Glide
 import com.example.acarreo_app.R
 
 class ThirdActivity : AppCompatActivity() {
@@ -11,11 +13,15 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
+        showGif()
         val module1Button = findViewById<AppCompatButton>(R.id.module1Button)
-
         module1Button.setOnClickListener{
             val scanScreen = Intent(this, nfcActivity::class.java)
             startActivity(scanScreen)
         }
+    }
+    fun showGif() {
+        val imageView:ImageView = findViewById(R.id.nfcGif)
+        Glide.with(this).load(R.drawable.nfc).into(imageView)
     }
 }
