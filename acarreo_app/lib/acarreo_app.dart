3a297d5b070b42b24f96b1770c615cd88e54e109
Ariel Rios <arturi.marking@gmail.com>
2023-09-06 22:21:49 -0600

    fix: acarreo_app

---
+++ acarreo_app/lib/acarreo_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AcarreoApp extends StatelessWidget {
+  const AcarreoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
