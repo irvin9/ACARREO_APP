5a7255e9a268354cfd0006cd8034332520fe37d5
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-09-19 13:22:54 -0600

    Rename read buffers and change baudrate

--- nfc-programmer/NFC_READER/ncfTest/src/main.cpp
+++ nfc-programmer/NFC_READER/ncfTest/src/main.cpp
@@ -29,9 +29,11 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 byte writeBuffer[34];
+byte readBuffer1[18];
+byte readBuffer2[18];
 
 void setup() {
-  Serial.begin(9600);        // Initialize serial communications with the PC
+  Serial.begin(115200);        // Initialize serial communications with the PC
   SPI.begin();               // Init SPI bus
   mfrc522.PCD_Init();        // Init MFRC522 card
   Serial.println(F("Write personal data on a MIFARE PICC "));
@@ -154,8 +156,6 @@ void loop() {
 
   Serial.print(F("Name: "));
 
-  byte buffer1[18];
-
   block = 4;
   len = 18;
 
@@ -167,7 +167,7 @@ void loop() {
     return;
   }
 
-  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  status = mfrc522.MIFARE_Read(block, readBuffer1, &len);
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Reading failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
@@ -177,16 +177,15 @@ void loop() {
   //PRINT FIRST NAME
   for (uint8_t i = 0; i < 16; i++)
   {
-    if (buffer1[i] != 32)
+    if (readBuffer1[i] != 32)
     {
-      Serial.write(buffer1[i]);
+      Serial.write(readBuffer1[i]);
     }
   }
   Serial.print(" ");
 
   //---------------------------------------- GET LAST NAME
 
-  byte buffer2[18];
   block = 1;
 
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
@@ -196,7 +195,7 @@ void loop() {
     return;
   }
 
-  status = mfrc522.MIFARE_Read(block, buffer2, &len);
+  status = mfrc522.MIFARE_Read(block, readBuffer2, &len);
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Reading failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
@@ -205,7 +204,7 @@ void loop() {
 
   //PRINT LAST NAME
   for (uint8_t i = 0; i < 16; i++) {
-    Serial.write(buffer2[i] );
+    Serial.write(readBuffer2[i] );
   }
 
 
