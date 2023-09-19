276d2550c0e2de664f174db04c1946c63b7519df
Ariel Rios <arturi.marking@gmail.com>
2023-09-19 08:58:51 -0600

    fix: disable token and middleware

--- acarreo_core_backend/src/api/app/Midlewares/auth.py
+++ acarreo_core_backend/src/api/app/Midlewares/auth.py
@@ -11,21 +11,22 @@ from api.app.Services.ChecadoresService import ChecadoresService
 def auth_midleware(func):
     @wraps(func)
     def decorator_func(*args, **kwargs):
-        auth_token = parser_token()
-        if auth_token:
-            try:
-                data = decode_token(auth_token)
-                current_user = find(ChecadoresService(), data["id"])
-                if current_user.status_code != HTTPStatusCode.OK.value:
-                    raise APIException(
-                        "Invalid Authentication token!",
-                        HTTPStatusCode.UNAUTHORIZED.value,
-                    )
-                return func(*args, **kwargs)
-            except Exception as e:
-                print(e)
-                raise APIException("Bad token", HTTPStatusCode.UNAUTHORIZED.value)
+        return func(*args, **kwargs)
+        # auth_token = parser_token()
+        # if auth_token:
+        #     try:
+        #         data = decode_token(auth_token)
+        #         current_user = find(ChecadoresService(), data["id"])
+        #         if current_user.status_code != HTTPStatusCode.OK.value:
+        #             raise APIException(
+        #                 "Invalid Authentication token!",
+        #                 HTTPStatusCode.UNAUTHORIZED.value,
+        #             )
+        #         return func(*args, **kwargs)
+        #     except Exception as e:
+        #         print(e)
+        #         raise APIException("Bad token", HTTPStatusCode.UNAUTHORIZED.value)
 
-        raise APIException("There is no token in headers", HTTPStatusCode.UNAUTHORIZED.value)
+        # raise APIException("There is no token in headers", HTTPStatusCode.UNAUTHORIZED.value)
 
     return decorator_func
