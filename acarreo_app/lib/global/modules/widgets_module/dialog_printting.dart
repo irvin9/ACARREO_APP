adee9fefd630289d1900adb1238838e2821c7c33
Ariel Rios <arturi.marking@gmail.com>
2023-09-27 23:40:52 -0600

    feat: add states for printting.

--- acarreo_app/lib/global/modules/widgets_module/dialog_printting.dart
+++ acarreo_app/lib/global/modules/widgets_module/dialog_printting.dart
@@ -13,49 +13,127 @@ class DialogPritting {
   static show(BuildContext context, DialogMessageModel message) {
     return GenericDialog.show(
       context: context,
-      child: AlertDialog(
-        icon: const Icon(Icons.print, size: 60, color: Colors.black87),
-        actions: [
-          GeneralButton(
-            fontSize: 18.0,
-            buttonText: _printerCubit.selectedPrinter != null
-                ? 'Imprimir'
-                : 'Seleccionar impresora',
-            textColor: Colors.black87,
-            onPressed: _printerCubit.selectedPrinter != null
-                ? () => _printerCubit.print(_acarreoCubit.formatTicket())
-                : () {
-                    hide(context);
-                    DialogSearchPrinter.show(context);
-                  },
-          ),
-          const SizedBox(height: 10.0),
-          GeneralButton(
-            buttonText: 'No imprimir',
-            textColor: Colors.white,
-            buttonColor: Colors.black87,
-            onPressed: () =>
-                _acarreoCubit.finishForm(GlobalRoutesApp.registerTravelRoute),
+      child: BlocBuilder<PrinterCubit, PrinterState>(
+        builder: (context, state) {
+          return _buildBody(context, state, message);
+        },
+      ),
+    );
+  }
+
+  static _buildBody(
+      BuildContext context, PrinterState state, DialogMessageModel message) {
+    switch (state) {
+      case PrinterErrorPrint():
+        return _buildErrorBody();
+      case PrinterSuccessPrint():
+        return _buildSuccessBody();
+      case PrinterInitPrint():
+        return const LoaderInnerDialog(
+            description: 'Estamos imprimiendo su ticket...');
+      default:
+        return _buildInitBody(context, message);
+    }
+  }
+
+  static _buildErrorBody() {
+    return AlertDialog(
+      icon:
+          const Icon(Icons.error_outline_outlined, size: 60, color: Colors.red),
+      actions: [
+        GeneralButton(
+          buttonText: 'Reintentar',
+          textColor: Colors.white,
+          buttonColor: Colors.black87,
+          onPressed: () => _printerCubit.print(_acarreoCubit.formatTicket()),
+        ),
+      ],
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Ha ocurrido algo con la impresión, vuelva intentarlo.',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(
+                fontSize: 14.0, fontWeight: FontWeight.w400),
           ),
         ],
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              message.title,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                  fontSize: 20.0, fontWeight: FontWeight.w700),
-            ).withPaddingOnly(bottom: 12),
-            Text(
-              message.description,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                  fontSize: 14.0, fontWeight: FontWeight.w400),
-            ),
-          ],
+      ),
+    );
+  }
+
+  static _buildSuccessBody() {
+    return AlertDialog(
+      icon:
+          const Icon(Icons.check_circle_outline, size: 60, color: Colors.green),
+      actions: [
+        GeneralButton(
+          buttonText: 'Finalizar',
+          textColor: Colors.white,
+          buttonColor: Colors.black87,
+          onPressed: () =>
+              _acarreoCubit.finishForm(GlobalRoutesApp.registerTravelRoute),
         ),
+      ],
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Se ha compleado la impresión de forma exitosa.',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(
+                fontSize: 14.0, fontWeight: FontWeight.w400),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static _buildInitBody(BuildContext context, DialogMessageModel message) {
+    return AlertDialog(
+      icon: const Icon(Icons.print, size: 60, color: Colors.black87),
+      actions: [
+        GeneralButton(
+          fontSize: 18.0,
+          buttonText: _printerCubit.selectedPrinter != null
+              ? 'Imprimir'
+              : 'Seleccionar impresora',
+          textColor: Colors.black87,
+          onPressed: _printerCubit.selectedPrinter != null
+              ? () => _printerCubit.print(_acarreoCubit.formatTicket())
+              : () {
+                  hide(context);
+                  DialogSearchPrinter.show(context);
+                },
+        ),
+        const SizedBox(height: 10.0),
+        GeneralButton(
+          buttonText: 'No imprimir',
+          textColor: Colors.white,
+          buttonColor: Colors.black87,
+          onPressed: () =>
+              _acarreoCubit.finishForm(GlobalRoutesApp.registerTravelRoute),
+        ),
+      ],
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            message.title,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(
+                fontSize: 20.0, fontWeight: FontWeight.w700),
+          ).withPaddingOnly(bottom: 12),
+          Text(
+            message.description,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(
+                fontSize: 14.0, fontWeight: FontWeight.w400),
+          ),
+        ],
       ),
     );
   }
