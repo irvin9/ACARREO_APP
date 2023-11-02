d8972592347b18ca6a31e63d6ce064f40c7de97c
Ariel Rios <arturi.marking@gmail.com>
2023-11-01 22:18:11 -0600

    feat: NFC Read and Write on repository

--- acarreo_app/lib/global/core/data/repository/scan_nfc_manager_repository.dart
+++ acarreo_app/lib/global/core/data/repository/scan_nfc_manager_repository.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:acarreo_app/global/core/domain/repository/scan_nfc_repository.dart';
 import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
+import 'package:ndef/ndef.dart' as ndef;
 
 class ScanNFCManagerRepository implements ScanNfcRepository {
   late NFCAvailability _isAvailable;
+  late NFCTag _tag;
 
   NFCAvailability get isAvailable => _isAvailable;
 
@@ -20,10 +24,9 @@ class ScanNFCManagerRepository implements ScanNfcRepository {
     String? tagDecode;
     bool checkSupport = await isSupported();
     if (checkSupport) {
-      final tag = await FlutterNfcKit.poll(timeout: Duration(seconds: timeout))
+      _tag = await FlutterNfcKit.poll(timeout: Duration(seconds: timeout))
           .timeout(Duration(seconds: timeout));
-      tagDecode = tag.id;
-      await stopSession();
+      tagDecode = _tag.id;
     }
     return tagDecode;
   }
@@ -31,5 +34,33 @@ class ScanNFCManagerRepository implements ScanNfcRepository {
   @override
   Future<void> stopSession() async {
     await FlutterNfcKit.finish();
+  }
+
+  @override
+  Future<Map<String, dynamic>?> readNfcData() async {
+    final ndefAvailable = _tag.ndefAvailable ?? false;
+    if (ndefAvailable) {
+      final records = await FlutterNfcKit.readNDEFRecords(cached: false);
+      if (records.isNotEmpty) {
+        final record = records.first;
+        final payload = record.payload;
+        if (payload != null) {
+          String recordData = utf8.decode(payload.buffer.asInt8List().toList());
+          final data = json.decode(recordData);
+          return data;
+        }
+      }
+    }
+    return null;
+  }
+
+  @override
+  Future<void> writeNfcData(Map<String, dynamic> data) async {
+    final bool isWritable = (_tag.type == NFCTagType.mifare_ultralight ||
+            _tag.type == NFCTagType.mifare_classic) &&
+        _tag.ndefWritable == true;
+    if (!isWritable) return;
+    final jsonData = jsonEncode(data);
+    await FlutterNfcKit.writeNDEFRecords([ndef.TextRecord(text: jsonData)]);
   }
 }
--- acarreo_app/lib/global/core/domain/repository/scan_nfc_repository.dart
+++ acarreo_app/lib/global/core/domain/repository/scan_nfc_repository.dart
@@ -2,4 +2,7 @@ abstract class ScanNfcRepository {
   Future<bool> isSupported();
   Future<String?> startSession();
   Future<void> stopSession();
+
+  Future<void> writeNfcData(Map<String, dynamic> data);
+  Future<Map<String, dynamic>?> readNfcData();
 }
