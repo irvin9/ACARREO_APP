3a2fb9e791b4890c790beb442c88493de2e8baed
Jonathan-Burgos-Sustaita <j.burgos.sustaita@gmail.com>
2023-11-21 00:10:06 -0600

    Feat: Show warning when details_ticket_travel fields are not filled

--- acarreo_app/lib/global/modules/tracker_module/core/ui/screens/details_ticket_travel_screen.dart
+++ acarreo_app/lib/global/modules/tracker_module/core/ui/screens/details_ticket_travel_screen.dart
@@ -2,11 +2,12 @@ import 'package:acarreo_app/global/core/acarreo_core_module.dart';
 import 'package:acarreo_app/global/modules/tracker_module/core/ui/widgets/details_ticket_form.dart';
 import 'package:acarreo_app/global/modules/tracker_module/core/ui/widgets/general_tracker_wrap.dart';
 import 'package:acarreo_app/global/modules/widgets_module/widgets_module.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class DetailsTicketTravelScreen extends StatelessWidget {
   final int? currentStep;
   final int? totalSteps;
+
   const DetailsTicketTravelScreen({
     super.key,
     this.currentStep,
@@ -26,6 +27,10 @@ class DetailsTicketTravelScreen extends StatelessWidget {
       onContinue: () {
         if (formKey.currentState!.validate()) {
           Modular.to.navigate(GlobalRoutesApp.previewTicketTravelRoute);
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Complete los campos faltantes!')),
+          );
         }
       },
       disableToBack: true,
