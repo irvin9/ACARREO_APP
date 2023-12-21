f3bb02f7ee5df2048d63c48d85122dd16a567d70
Ariel Rios <arturi.marking@gmail.com>
2023-12-21 00:04:48 -0600

    feat(app): add get current user attribute

--- acarreo_app/lib/global/core/domain/cubit/acarreo/acarreo_cubit.dart
+++ acarreo_app/lib/global/core/domain/cubit/acarreo/acarreo_cubit.dart
@@ -13,6 +13,8 @@ class AcarreoCubit extends Cubit<AcarreoState> {
 
   final AcarreoDataManagerService managerService;
 
+  final StorageService storage = Modular.get<StorageService>();
+
   final int totalSteps = 5;
 
   final Map<String, dynamic> _formAnswers = {};
