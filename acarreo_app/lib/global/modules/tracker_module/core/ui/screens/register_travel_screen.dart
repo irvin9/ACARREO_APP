4a172b6627feb8a80f4b27d2670644d299d81f5b
Ariel Rios <arturi.marking@gmail.com>
2024-01-02 11:27:08 -0600

    feat: refactor registrar travel screen

--- acarreo_app/lib/global/modules/tracker_module/core/ui/screens/register_travel_screen.dart
+++ acarreo_app/lib/global/modules/tracker_module/core/ui/screens/register_travel_screen.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:acarreo_app/global/core/acarreo_core_module.dart';
 import 'package:acarreo_app/global/modules/widgets_module/widgets_module.dart';
-import 'package:acarreo_app/global/modules/tracker_module/core/ui/widgets/general_tracker_wrap.dart';
 import 'package:acarreo_app/global/modules/tracker_module/core/ui/widgets/register_travel_form.dart';
 
 class RegisterTravelScreen extends StatelessWidget {
-  final int currentStep;
-
   const RegisterTravelScreen({
     super.key,
-    this.currentStep = 1,
   });
 
   @override
@@ -26,31 +22,21 @@ class RegisterTravelScreen extends StatelessWidget {
     const String titleH2 = 'Detalles de la ubicación';
     const String descriptionH2 = 'Añade los detalles de tu ubicación';
 
-    return GeneralTrackerWrap(
-      currentStep: currentStep,
-      onContinue: () {
-        if (formKey.currentState!.validate()) {
-          cubit.saveFirsLocation();
-          Modular.to
-              .navigate(GlobalRoutesApp.previewCurrentLocationTravelRoute);
-        }
-      },
-      children: [
-        const TitleForm(
-          title: title,
-          description: description,
-        ),
-        const SizedBox(height: 8.0),
-        TitleForm.h2(
-          title: titleH2,
-          description: descriptionH2,
-        ),
-        const SizedBox(height: 12.0),
-        if (currentUser.idModule == 0)
-          RegisterTravelForm(formKey: formKey)
-        else
-          RegisterTravelBankForm(formKey: formKey)
-      ],
-    );
+    return [
+      const TitleForm(
+        title: title,
+        description: description,
+      ),
+      const SizedBox(height: 8.0),
+      TitleForm.h2(
+        title: titleH2,
+        description: descriptionH2,
+      ),
+      const SizedBox(height: 12.0),
+      if (currentUser.idModule == 0)
+        RegisterTravelForm(formKey: formKey)
+      else
+        RegisterTravelBankForm(formKey: formKey)
+    ].setSingleScrollAdaptable();
   }
 }
