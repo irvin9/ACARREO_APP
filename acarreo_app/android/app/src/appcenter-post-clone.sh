a64b14a5dcd0f5a914411761d57643025c62cf4d
Ariel Rios <arturi.marking@gmail.com>
2024-01-26 02:29:35 -0600

    feat: add automation app center build appbundle

---
+++ acarreo_app/android/app/src/appcenter-post-clone.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+#Place this script in project/android/app/
+
+cd ..
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+cd ..
+# Crear el contenido del archivo JSON utilizando variables de entorno o sistema
+CONFIG_JSON=$(
+    cat <<EOF
+{
+"ENVIRONMENT": "${ENVIRONMENT}",
+  "APP_NAME": "${APP_NAME}",
+  "APP_BUNDLE": "${APP_BUNDLE}",
+  "APP_SUFFIX": "${APP_SUFFIX}",
+  "ACARRO_API_HOST_URL": "${ACARRO_API_HOST_URL}",
+  "ACARREO_API_VERSION": "${ACARREO_API_VERSION}",
+  "FIREBASE_API_KEY_IOS": "${FIREBASE_API_KEY_IOS}",
+  "FIREBASE_APP_ID_IOS": "${FIREBASE_APP_ID_IOS}",
+  "FIREBASE_API_KEY_ANDROID": "${FIREBASE_API_KEY_ANDROID}",
+  "FIREBASE_APP_ID_ANDROID": "${FIREBASE_APP_ID_ANDROID}",
+  "FIREBASE_SENDER_ID": "${FIREBASE_SENDER_ID}",
+  "FIREBASE_PROJECT_ID": "${FIREBASE_PROJECT_ID}",
+  "FIREBASE_STORAGE_BUCKET": "${FIREBASE_STORAGE_BUCKET}"
+}
+EOF
+)
+
+# Guardar el contenido en un archivo config.json
+echo "$CONFIG_JSON" >config.json
+
+echo "Archivo config.json creado con éxito."
+
+last_version=$(git describe --match "v[0-9]*.[0-9]*.[0-9]*+[0-9]*" --abbrev=0 --tags)
+numberBuild="${version#*+}"
+
+if [ "$ENVIRONMENT" == "dev" ]; then
+    echo "El actual número de build es: $numberBuild"
+    # Verificar si el número es un entero antes de sumar uno
+    if [[ $numberBuild =~ ^[0-9]+$ ]]; then
+        # Sumar uno al número de build
+        numberBuild=$((numberBuild + 1))
+        # Construir la nueva versión con el nuevo número de build
+        newVersion=$(echo $version | sed "s/+${numberBuild}/+${newNumberBuild}/")
+
+        # Etiquetar el repositorio Git con la nueva versión
+        git checkout dev
+        git tag -a "$newVersion" -m "feat: new version app $newVersion"
+        git push origin dev --tags
+        echo "Versión actualizada y repositorio etiquetado con $newVersion"
+    else
+        echo "Error: El número de build no es un entero válido."
+    fi
+else
+    echo "Tag de $ENVIRONMENT -> $last_version"
+fi
+
+# Configuración para revertir en caso de error
+trap 'handle_error' ERR
+
+# Función para revertir el tag creado y salir con un mensaje de error
+handle_error() {
+    if [ "$ENVIRONMENT" == "dev" ]; then
+        echo "Error detectado. Revertir el tag creado."
+        git tag -d "$newVersion"
+        exit 1
+    else
+        echo "Error detectado."
+    fi
+}
+
+git clone -b beta https://github.com/flutter/flutter.git
+export PATH=$(pwd)/flutter/bin:$PATH
+
+flutter channel stable
+dart pub global activate fvm
+fvm use 3.13.0
+fvm global 3.13.0
+flutter doctor
+
+echo "Installed flutter to $(pwd)/flutter"
+
+# build APK
+# if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
+# flutter build apk --debug
+# flutter build apk --profile
+# flutter build apk --release
+flutter build appbundle --flavor "$ENVIRONMENT" --dart-define-from-file=config.json
+
+# if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
+#flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
+
+# copy the APK where AppCenter will find it
+# mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+
+# copy the AAB where AppCenter will find it
+mkdir -p android/app/build/outputs/bundle/
+mv build/app/outputs/bundle/"$ENVIRONMENT"Release/app-"$ENVIRONMENT"-release.aab $_
+
+# Desactivar la configuración de revertir en caso de error después de este punto
+trap - ERR
