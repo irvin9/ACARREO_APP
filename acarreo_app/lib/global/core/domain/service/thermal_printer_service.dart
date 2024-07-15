7639090165f660bab36bf018fbd7694dcee2b3f1
Ariel Rios <arturi.marking@gmail.com>
2024-07-15 12:26:58 -0600

    fix: change interface generic type unnecesary

--- acarreo_app/lib/global/core/domain/service/thermal_printer_service.dart
+++ acarreo_app/lib/global/core/domain/service/thermal_printer_service.dart
@@ -1,6 +1,7 @@
 import 'package:acarreo_app/global/core/domain/models/thermal_printer_device.dart';
 
-abstract class ThermalPrinterService<T> {
+abstract class ThermalPrinterService {
   Future<List<ThermalPrinterDevice>?> getPrinters();
-  Future<bool> print(T printer, Map<String, dynamic> data);
+  Future<bool> print(ThermalPrinterDevice printer, Map<String, dynamic> data);
+  Future<bool> disconnect();
 }
